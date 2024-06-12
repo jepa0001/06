c492f432c1ed65b030487179a7d03b09f4e8eeed
jepa0001 <159116066+jepa0001@users.noreply.github.com>
2024-06-12 04:14:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", briller);
+
+function briller() {
+  console.log("solbriller");
+  document.querySelector("#briller_container").classList.add("hop");
+}
